4328dbff0fa19360de47852037991a9298db57a9
Patrick Wilmes <p.wilmes89@gmail.com>
2024-10-30 22:11:16 +0100

    Initial setup

---
+++ src/main/kotlin/com/bitlake/Main.kt
@@ -0,0 +1,11 @@
+/*
+* Copyright (c) 2024, Patrick Wilmes <p.wilmes89@gmail.com>
+* All rights reserved.
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+package com.bitlake
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
