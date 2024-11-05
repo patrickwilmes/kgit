fd75d01b051b6721fa2f6553ab00a47b88bc8b9e
Patrick Wilmes <p.wilmes89@gmail.com>
2024-11-05 20:02:14 +0100

    Main entrypoint
    Implementing a basic main entrypoint for the kgit application, to be started with. Right now it's pretty straight forward and simple, by only checking if there are command line arguments, otherwise presenting the caller with an error message and usage instructions.
    Right now the main will only create and execute a command.

--- src/main/kotlin/com/bitlake/Main.kt
+++ src/main/kotlin/com/bitlake/Main.kt
@@ -6,6 +6,25 @@
 */
 package com.bitlake
 
+import arrow.core.raise.either
+import com.bitlake.command.CommandFactory
+
+/*
+kgit <COMMAND> <ARGS>
+kgit init | [OPTIONAL_ARGS] (path)
+ */
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    if (args.isEmpty()) {
+        println("Usage: kgit <COMMAND> <ARGS>")
+        return
+    }
+    either {
+        val command = CommandFactory.fromArgs(args).bind()
+        command.execute().bind()
+    }.fold({
+        println(it.message)
+    }) {
+        it.render()
+    }
 }
