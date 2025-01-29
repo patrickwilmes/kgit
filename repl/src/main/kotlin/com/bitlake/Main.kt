ab42024c40e8681408110c4dcbc596c82bf85a74
Patrick Wilmes <p.wilmes89@gmail.com>
2025-01-29 19:17:01 +0100

    Making repl functional equal to cli
    The repl wasn't hooking up all the ak's functionality, which is now
    fixed by this commit. The repl behaves exactly like the cli now,
    just interactive.

--- repl/src/main/kotlin/com/bitlake/Main.kt
+++ repl/src/main/kotlin/com/bitlake/Main.kt
@@ -1,12 +1,24 @@
 package com.bitlake
 
+import arrow.core.raise.either
+import com.bitlake.command.CommandFactory
+
 private fun prompt() {
     print("> ")
 }
 
+private fun String.sanitize() = split(" ").toTypedArray()
+
 fun main() {
     do {
         prompt()
-        val input = readlnOrNull()
-    } while (input != null && input != "exit")
+        val input = readlnOrNull()?.sanitize()
+        either {
+            val command = CommandFactory.fromArgs(input ?: emptyArray<String>()).bind()
+            val renderer = command.execute().bind()
+            renderer.render()
+        }.onLeft {
+            println(it.message)
+        }
+    } while (input != null && input[0] != "exit")
 }
